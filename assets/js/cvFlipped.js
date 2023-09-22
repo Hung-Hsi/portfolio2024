608017bbe9f382bb83e48c6466aa52301e19d668
Jogle Chang <jogle chang@gmail.com>
2023-09-23 00:11:12 +0800

    cv flipped improve

--- assets/js/cvFlipped.js
+++ assets/js/cvFlipped.js
@@ -1,6 +1,6 @@
 $('.cvDownload').on('click', function (e) {
     // console.log(e.target.classList);
-    if (e.target.classList.contains('card') || e.target.classList.contains('cvDownload')) {
+    if (e.target.classList.contains('card') || e.target.classList.contains('cvDownload') || e.target.classList.contains('cardContainer')){
         $('.cardContainer').toggleClass('flipped');
     }
 });
